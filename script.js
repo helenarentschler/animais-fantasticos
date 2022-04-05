01a72585cb8f622e18c5a2cae311aedfaacffae3
Helena Rentschler <helrentschler@gmail.com>
2022-04-05 17:49:45 -0300

    animação ao scroll

--- script.js
+++ script.js
@@ -72,3 +72,27 @@ function initSmoothScroll() {
 };
 
 initSmoothScroll();
+
+function initAnimacaoScroll() {
+    const sections = document.querySelectorAll('.js-scroll');
+    const windowMetade = window.innerHeight * 0.6;
+
+    if (sections.length) {
+        function animaScroll() {
+            sections.forEach((section) =>{
+                const sectionTop = section.getBoundingClientRect().top;
+                const isSectionVisible = (sectionTop - windowMetade) < 0;
+                if (isSectionVisible) 
+                    section.classList.add('ativo');
+                else
+                    section.classList.remove('ativo');
+            });
+        };
+        animaScroll();
+        window.addEventListener('scroll', animaScroll);
+    };
+};
+
+initAnimacaoScroll();
+    
+    
